a5fb68b494a9f7c21314238d0c10903cf8d95600
AMAMI <36912991+AMAMI-Monolith@users.noreply.github.com>
2022-03-02 15:12:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,8 @@ from os import getenv
 import traceback
 
 bot = commands.Bot(command_prefix='/')
-
+intents=discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -13,8 +14,19 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def sakuya(ctx):
+    await ctx.send('こんにちは、私の名前は白瀬咲耶。')
+    
+@client.event
+async def on_member_join(member):
+    guild = member.guild
+    channel=discord.utils.get(guild.text_channels, name="beta_welcome")
+    embed=discord.Embed(title=f"{member.author.name}さんが参加しました", url="https://bit.ly/2JahfiF", color=0x00ffff)
+    embed.set_thumbnail(url="https://nureyon.com/sample/84/check_mark-2-p53.svg?2020-09-09")
+    embed.add_field(name="参加者", value="f{member.author.mention}", inline=False)
+    embed.set_footer(text="サーバー参加を検知しました")
+    await channel.send(f'{member.author.mention}',embed=embed)
+
 
 
 token = getenv('DISCORD_BOT_TOKEN')
