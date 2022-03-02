1941378fe78bc5a8dd013b9a3b56c395c45b8000
AMAMI <36912991+AMAMI-Monolith@users.noreply.github.com>
2022-03-02 23:05:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,12 @@
+# coding: utf-8
+
 # インストールした discord.py を読み込む
 import discord
 from discord.ext import commands
 
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = 'OTQ4NDQ1Mzc3MjM1OTMxMjA4.Yh76lw.K5DHomY8LQVirPKqa10JVqu14-8'
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(prefix='!')
 
 #bot = discord.Client()
 #client.Bot(command_prefix='/',activity=discord.Activity(name='起動中!',type=discord.ActivityType.watching))
@@ -21,7 +23,7 @@ async def on_ready():
     embed = discord.Embed( # Embedを定義する
                           title="Hello Sakuya",# タイトル
                           color=0x00ff00, # フレーム色指定(今回は緑)
-                          description="ログインしたよ。コマンドは'!'をはじめに入力してね。",
+                          description="ログインしたよ。コマンドは【!】をはじめに入力してね。",
                           )
     embed.set_author(name=bot.user, # Botのユーザー名
                      icon_url=bot.user.avatar_url # Botのアイコンを設定してみる
@@ -34,15 +36,20 @@ async def on_ready():
     await channel.send(embed=embed) # embedの送信には、embed={定義したembed名}
 #新規メンバー参加時に実行されるイベントハンドラ
 
+#あいさつするイベントハンドラ
+@bot.command(name="こんにちは")
+async def hello(ctx):
+    await ctx.send(f"どうも、{ctx.message.author.name}さん！")
+
 #テキストチャンネル内のログが消える
 @bot.event
 async def on_message(message):
     if message.content == '!cleanup':
         if message.author.guild_permissions.administrator:
             await message.channel.purge()
-            await message.channel.send('キレイになったよ、プロデューサー。')
+            await message.channel.send('塵一つ残らないね！')
         else:
-            await message.channel.send('ごめんね、このコマンドは管理者にしか使えないんだ。')
+            await message.channel.send('何様のつもり？')
     elif message.content == '!hello':
         await message.channel.send('こんにちは、プロデューサー。')
 
