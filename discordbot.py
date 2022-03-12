6335e6e07d39580737278f60226f94f1129b3b14
あまみ / AMAMI <36912991+AMAMI-Monolith@users.noreply.github.com>
2022-03-12 16:49:48 +0900

    安定版リリースv0

--- discordbot.py
+++ discordbot.py
@@ -1,40 +1,43 @@
 import discord
 import traceback
 from discord.ext import commands
+from discord.utils import get
 from os import getenv
 
 # Botの起動とDiscordサーバーへの接続
 bot = commands.Bot(command_prefix = "!")
+ADMIN_ID = '260333442489647105'
 token = getenv('DISCORD_BOT_TOKEN')
- 
+
 @bot.command()
 async def nya(ctx):
     """ テスト:nyaa """
-    if message.author.bot:
-        return
-    else:
-        await ctx.send('にゃー')
+    await ctx.send('にゃー')
 
 
 @bot.command()
 async def hello(ctx):
     """ 挨拶を返す """
-    reply = f'こんにちは、{ctx.author.mention}プロデューサー。'
-    await ctx.channel.send(reply)
+    if message.author.bot:
+        return
+    else:
+        reply = f'こんにちは、{ctx.author.mention}プロデューサー。'
+        await ctx.channel.send(reply)
 
 
 @bot.command()
 async def site(ctx):
     """ 公式サイトへの案内 """
-    embed=discord.Embed(
-                        title='シャイニーカラーズ',
-                        url='https://shinycolors.idolmaster.jp/',
-                        description='公式サイトはこちらから',
-                        color=0x00f900)
-    embed.set_thumbnail(
-                        url='https://shinycolors.idolmaster.jp/pc/static/img/download/thumb_lantica_sakuya.png'
-                        )
-    await ctx.send(embed=embed)
+    if message.author.bot:
+        return
+    else:
+        embed=discord.Embed(
+                            title='シャイニーカラーズ',
+                            url='https://shinycolors.idolmaster.jp/',
+                            description='公式サイトはこちらから',
+                            color=0x00f900)
+        embed.set_thumbnail(url='https://shinycolors.idolmaster.jp/pc/static/img/download/thumb_lantica_sakuya.png')
+        await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -44,7 +47,7 @@ async def cleanup(ctx):
         await ctx.channel.purge()
         await ctx.channel.send('チャンネルを綺麗にしたよ。')
     else:
-        await ctx.channel.send('管理者以外は使用できないよ。')
+        await ctx.channel.send('管理者専用コマンドだよ。')
 
 
 @bot.event
@@ -55,13 +58,17 @@ async def create_channel(message, channel_name):
     new_channel = await category.create_text_channel(name=channel_name)
     return new_channel
 
+
 @bot.command()
 async def mkch(ctx):
     """ 発言したチャンネルのカテゴリ内に新規テキストチャンネルを作成。 """
-    new_channel = await create_channel(ctx, channel_name = ctx.author.name)
-    # チャンネルのリンクと作成メッセージを送信
-    text = f'{new_channel.mention} を作成したよ。'
-    await ctx.channel.send(text)
+    if message.author.bot:
+        return
+    else:
+        new_channel = await create_channel(ctx, channel_name = ctx.author.name)
+        # チャンネルのリンクと作成メッセージを送信
+        text = f'{new_channel.mention} を作成したよ。'
+        await ctx.channel.send(text)
 
 
 @bot.event
@@ -78,14 +85,39 @@ async def on_member_join(member):
                         embed=embed)
 
 
+@bot.command()
+async def stop(message):
+    """ Botを停止することができる(管理者のみ) """
+    if message.author.guild_permissions.administrator:
+        admin = await bot.fetch_user(ADMIN_ID)
+        msg = "🌙 status : Offline"
+        await admin.send(msg)
+        await bot.logout()
+    else:
+        await message.channel.send("管理者専用コマンドだよ。")
+
+
 @bot.event
 async def on_ready():
-    # このbotのサーバーにオンラインになった時に私にDM(ダイレクトメッセージ)を送信する。
+    # このbotのサーバーにオンラインになった時に管理人にDM(ダイレクトメッセージ)を送信する。
     print('------')
     print('Login infomation>>>')
     print(f'{bot.user.name}がログインしたよ。')
     print('------')
     await bot.change_presence(activity=discord.Game(name="!help"))
+    admin = await bot.fetch_user(ADMIN_ID)
+    msg = "🔴 status : Online"
+    await admin.send(msg)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+bot.run('OTQ4NDQ1Mzc3MjM1OTMxMjA4.Yh76lw.K5DHomY8LQVirPKqa10JVqu14-8')
 
 
 bot.run(token)
