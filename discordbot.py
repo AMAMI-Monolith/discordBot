f570e660ed6cf9a5384a7744adc1b271b4b46b84
あまみ / AMAMI <accelerator_1030@yahoo.co.jp>
2022-03-15 10:09:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -42,24 +42,25 @@ async def site(message):
                             description='公式サイトはこちらから',
                             color=0x00f900
         )
+        embed.set_thumbnail(url='https://shinycolors.idolmaster.jp/pc/static/img/download/thumb_lantica_sakuya.png')
+        await message.send(embed=embed)
         await buttons.send(
             "components": [
                 {
-                    "type": 1,
-                    "components": [
-                        {
-                            "style": 5,
-                            "label": `公式サイト`,
-                            "url": `https://shinycolors.idolmaster.jp/`,
-                            "disabled": false,
-                            "type": 2
-                        }
-                    ]
+                "type": 1,
+                "components": [
+                    {
+                    "style": 5,
+                    "label": `公式サイト`,
+                    "url": `https://shinycolors.idolmaster.jp/`,
+                    "disabled": false,
+                    "type": 2
+                    }
+                ]
                 }
             ]
         )
-        embed.set_thumbnail(url='https://shinycolors.idolmaster.jp/pc/static/img/download/thumb_lantica_sakuya.png')
-        await message.send(embed=embed)
+        
 
 
 @bot.command()
