b9986ec2edee3f6ff8e6d70a44c94b7179113940
あまみ / AMAMI <accelerator_1030@yahoo.co.jp>
2022-03-22 23:10:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,14 +8,14 @@ from os import getenv
 
 # Botの起動とDiscordサーバーへの接続
 ADMIN_ID = '260333442489647105'
+token = getenv('DISCORD_BOT_TOKEN')
 bot = commands.Bot(
     command_prefix = "!",
     case_insensitive= True, #コマンドの大文字小文字を無視する(True)
     help_command = None #標準のhelpコマンドを無効化する(None)
 )
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
 buttons = ButtonsClient(bot)
+slash = InteractionClient(bot)
 #-------------------------------
 
 
@@ -151,13 +151,13 @@ async def help(message):
         embed.set_image(url=f"attachment://{fname}") # embedに画像を埋め込むときのURLはattachment://ファイル名
         await message.channel.send(file=file, embed=embed) # ファイルとembedを両方添えて送信する
 
+
 #試験運用 bot.event -> client.event
 @client.event
 async def on_member_join(member):
     guild = member.guild
     channel=discord.utils.get(guild.text_channels, name="入室ログ")
     await channel.send(f'{member.author.mention}さん、ようこそ。\nまず、#はじめに #サーバールール をお読みください。')
-
 
 @bot.event
 async def on_member_join(member):
@@ -173,6 +173,5 @@ async def on_command_error(message, error):
 
 
 
-token = getenv('DISCORD_BOT_TOKEN')
 bot.run('OTQ4NDQ1Mzc3MjM1OTMxMjA4.Yh76lw.K5DHomY8LQVirPKqa10JVqu14-8')
 # bot.run(token)
