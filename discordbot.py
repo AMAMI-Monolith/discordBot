faf8fcd4396b02fc09a02a070cd98712a95a4c12
AMAMI-Monolith <36912991+AMAMI-Monolith@users.noreply.github.com>
2022-05-04 13:36:04 +0900

    public対応

--- discordbot.py
+++ discordbot.py
@@ -13,10 +13,10 @@ from dislash import InteractionClient, SelectMenu, SelectOption
 from os import getenv
 
 # Botの起動とDiscordサーバーへの接続
-ADMIN_ID = '260333442489647105'
+ADMIN_ID = '##admin_id'
 
 #token = getenv('DISCORD_BOT_TOKEN')
-token = 'OTQ4NDQ1Mzc3MjM1OTMxMjA4.Yh76lw.K5DHomY8LQVirPKqa10JVqu14-8'
+token = 'ThisT0ken'
 
 #-------------------------------
 intents = discord.Intents.all()
